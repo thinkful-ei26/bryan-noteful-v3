c90e881b3dbb77dd84a942637a02c4d9f1e4ecb3
Fantosism <businessspamfolder@gmail.com>
2018-11-27 13:37:39 -0700

    regexp for searchterm

--- scratch/queries.js
+++ scratch/queries.js
@@ -4,27 +4,28 @@ const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
 
 // Find/Search for notes using note.find
-// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-//   .then(() => {
-//     const searchTerm = 'lady gaga';
-//     let filter = {};
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const searchTerm = 'orci';
+    let filter = {};
 
-//     if (searchTerm) {
-//       filter.title = { $regex: searchTerm, $options: 'i' };
-//     }
+    if (searchTerm) {
+      filter = { $or: [{title : { $regex: searchTerm, $options: 'i' }},
+      {content : { $regex: searchTerm, $options: 'i'}}]
+      };
 
-//     return Note.find(filter).sort({ updatedAt: 'desc' });
-//   })
-//   .then(results => {
-//     console.log(results);
-//   })
-//   .then(() => {
-//     return mongoose.disconnect()
-//   })
-//   .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-//   });
+    return Note.find(filter).sort({ updatedAt: 'desc' });
+  }})
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect()
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
 
 // Find note by id using Note.findById
 
@@ -52,3 +53,27 @@ const Note = require('../models/note');
 //   .catch(error => console.log('SpaghettiOs'));
 
 // Update a note by id using Note.findByIdAndUpdate
+// mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
+//   .then(() => {
+//     let id = '000000000000000000000003';
+//     let updateTitle = 'Lady Gaga Hates Cats';
+//     return Note.findByIdAndUpdate({_id: id}, {$set: {title: updateTitle}}, {new: true}
+//     )
+//   })
+//   .then(result => console.log((result)))
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(error => console.log('SpaghettiOs'));
+
+// Delete a note by id using Note.findByIdAndRemove
+// mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
+//   .then(() => {
+//     let id = '000000000000000000000003';
+//     return Note.findByIdAndRemove(id)
+//    })
+//    .then(result => console.log(result))
+//    .then(() => {
+//      return mongoose.disconnect()
+//    })
+//    .catch(error => console.log('Zoinks'));
